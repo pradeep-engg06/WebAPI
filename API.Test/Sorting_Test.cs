0eb0035c331a2d4188f81a983b40ce0ae22fc657
Pradeep Shukla <pradeep.engg06@gmail.com>
2021-11-17 17:17:40 +0530

    Sort testing expected result change

--- API.Test/Sorting_Test.cs
+++ API.Test/Sorting_Test.cs
@@ -10,7 +10,7 @@ namespace API.Test
         }
 
         [Test]
-        [TestCase(new int[] { 12, 32, 45, 68, 15, 65 },new int[] {  15, 32, 45, 65, 68 })]
+        [TestCase(new int[] { 12, 32, 45, 68, 15, 65 },new int[] { 12, 15, 32, 45, 65, 68 })]
         [TestCase(new int[] { 32,65,78,44 }, new int[] { 32, 44, 65, 78 })]
         [TestCase(new int[] { 1,6,8,4,5,7,5,6,9,4,1,0,2,3 }, new int[] { 0, 1, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9 })]
         public void TestBubbleSortMethod(int[] input, int[] result)
